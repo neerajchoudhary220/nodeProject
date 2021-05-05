dfcb4584d5a534385f9b962d16b5ebd3092fd0c6
neerajchoudhary220 <myjobmylife783@gmail.com>
2021-05-05 21:25:24 +0530

    this is second oc

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const fs =require('fs');
 const os = require('os');
 const port = process.env.PORT||8000;
+
 var systemName = os.cpus();
 var computerType = os.platform();
 var ram = os.totalmem()/1024/1024/1024;
-fs.writeFileSync('my.txt','Computer Name:'+computerType+'\
-\nTotal Ram:'+ram+' GB');
+fs.writeFileSync('my.html','Computer Name:'+computerType+'\
+<br>Total Ram:'+ram+' GB');
+const myFile = fs.readFileSync('my.html');
 app.get("/",(req,res)=>
 {
     res.writeHead(200,{'Content-type':'text/html'});
@@ -15,6 +17,13 @@ app.get("/",(req,res)=>
     //console.log(myFile);
     res.send();
 });
+app.get("/my",(req,res)=>
+{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write(myFile);
+    //console.log(myFile);
+    res.send();
+})
 app.listen(port,()=>
 {
     console.log("server Running");
